3848374cecbf311fea9c9ec3caf5d8a1dd82e20b
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2020-08-12 20:09:29 +0200

    fix: add missing dependency

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,39 @@
+/**
+ * @module:@admin-bro/nestjs
+ * 
+ * @description
+ * This is an official plugin which allows you to render AdminBro in [NestJS
+ * framework](https://nestjs.com/)
+ * 
+ * ## Installation
+ * 
+ * 1. First of all, install the AdminBro along with the module:
+ * 
+ * ```
+ * yarn add admin-bro @admin-bro/nestjs
+ * ```
+ * 
+ * 2. Once the installation process is complete, we can import the AdminBroModule
+ * into the root AppModule.
+ * 
+ * ```
+ * 
+ * 
+ * 
+ * import { Module } from '@nestjs/common';
+ * import { AdminModule } from '@admin-bro/nestjs';
+ * 
+ * @Module({
+ *   imports: [
+ *     AdminModule.createAdminModule({
+ *       // module options
+ *     }),
+ *   ],
+ * })
+ * export class AppModule {}
+ * ```
+ */
+
 import * as NestJSPlugin from './admin.module'
 
 export * from './admin.module';
