d673b49d6f40b8bb0bc6e88b6424d2d198d782d3
wojtek-krysiak <wojciech.g.krysiak@gmail.com>
2020-08-12 21:10:33 +0200

    chore: add missing express-session package

--- src/index.ts
+++ src/index.ts
@@ -17,21 +17,94 @@
  * into the root AppModule.
  * 
  * ```
- * 
- * 
- * 
  * import { Module } from '@nestjs/common';
  * import { AdminModule } from '@admin-bro/nestjs';
  * 
  * @Module({
  *   imports: [
  *     AdminModule.createAdminModule({
- *       // module options
- *     }),
+ *      useFactory: () => ({
+ *        adminBroOptions: {
+ *          rootPath: '/admin',
+ *          resources: [],
+ *        },
+ *      }),
+ *    }),
  *   ],
  * })
  * export class AppModule {}
  * ```
+ * 
+ * Then enter `/admin` path in your browser and you should see the AdminBro.
+ * 
+ * 3. Passing resources
+ * 
+ * Let say you use @nestjs/typeorm module, and you have users module.
+ * 
+ * - you have to install @admin-bro/typeorm adapter
+ * - you have to register it in AdminBro (as stated in the docs)
+ * - and you have to pass it to your options
+ * 
+ * ```
+ * import AdminBro from 'admin-bro';
+ * import { Module } from '@nestjs/common';
+ * import { AdminModule } from '@admin-bro/nestjs';
+ * import { Database, Resource } from '@admin-bro/typeorm'
+ * import { TypeOrmModule } from '@nestjs/typeorm';
+ * import { UsersModule } from './users/users.module';
+ * 
+ * AdminBro.registerAdapter({ Database, Resource })
+ * 
+ * @Module({
+ *   imports: [
+ *     // you will have to change connection data of course :)
+ *     TypeOrmModule.forRoot({
+ *       type: 'postgres',
+ *       host: 'localhost',
+ *       port: 5432,
+ *       username: 'postgres',
+ *       password: '',
+ *       database: 'database_test',
+ *       entities: [User],
+ *       synchronize: true,
+ *     }),
+ *     AdminModule.createAdminModule({
+ *      useFactory: () => ({
+ *        adminBroOptions: {
+ *          rootPath: '/admin',
+ *          resources: [User],
+ *        },
+ *      }),
+ *    }),
+ *   ],
+ * })
+ * export class AppModule {}
+ * ```
+ * 
+ * ## Authentication
+ * 
+ * Apart from the `adminBroOptions` useFactory can return `auth` settings.
+ * 
+ * This is an example which always logs users in, since authenticate method
+ * always returns a Promise resolving to {@link CurrentAdmin}. You may
+ * want to compare the password against what what you have encrypted
+ * in the database.
+ * 
+ * ```
+ * AdminModule.createAdminModule({
+ *   useFactory: () => ({
+ *     adminBroOptions: {
+ *       rootPath: '/admin',
+ *       resources: [User],
+ *     },
+ *     auth: {
+ *       authenticate: async (email, password) => Promise.resolve({ email: 'test' }),
+ *       cookieName: 'test',
+ *       cookiePassword: 'testPass',
+ *     },
+ *   }),
+ * ```
+ * 
  */
 
 import * as NestJSPlugin from './admin.module'
