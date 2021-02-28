962fff1c1230708742de1e46c0316a3d7d54387a
Andrey Sakharov <sakharov.a@n-t.io>
2021-02-28 20:27:04 +0300

    move an existent example to a separate folder

--- example-app/mongoose/src/app.module.ts
+++ example-app/mongoose/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule, getModelToken } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
-import { AdminModule } from '../../src'; // lib
+import { AdminModule } from '../../../src'; // lib
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
