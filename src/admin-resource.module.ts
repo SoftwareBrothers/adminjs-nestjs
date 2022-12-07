a95fb707a8e3dd56564a1543279a52205d7996e8
Mathilde Duboille <duboille.m@gmail.com>
2022-12-07 10:08:24 +0100

    feat: allow registering admin entities inside their own module

---
+++ src/admin-resource.module.ts
@@ -0,0 +1,45 @@
+import { Module, DynamicModule } from '@nestjs/common';
+import { ResourceWithOptions } from 'adminjs'
+
+import AdminResourceService from './admin-resource.service';
+
+/**
+ * Nest module which is responsible for admin resources
+ * 
+ * @summary Nest Module
+ * 
+ * @class
+ * @name module:@adminjs/nestjs~AdminResourceModule
+ * @alias AdminResourceModule
+ * @memberof module:@adminjs/nestjs
+ */
+@Module({})
+export class AdminResourceModule {
+  /** 
+   * Register resources
+   * 
+   * @param {(ResourceWithOptions | any)[]} resources
+   * @memberof module:@adminjs/nestjs~AdminResourceModule
+   * @method
+   * @name forFeature
+   * @example
+   * import { Module } from '@nestjs/common';
+   * import { AdminResourceModule } from '@adminjs/nestjs';
+   * import { User } from './user.entity';
+   * 
+   * \@Module({
+   *   imports: [
+   *     AdminResourceModule.forFeature([User]),
+   *   ],
+   * })
+   * export class UserModule {}
+   * 
+   */
+  public static forFeature(resources: (ResourceWithOptions | any)[]): DynamicModule {
+    AdminResourceService.add(resources)
+    
+    return {
+      module: AdminResourceModule,
+    };
+  }
+}
